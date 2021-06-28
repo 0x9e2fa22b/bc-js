6baccc4048bd0c89ef946331c6f2cf5765fda856
CaoHuy26 <huyc197@gmail.com>
2021-06-28 22:16:16 +0700

    feat: transactions & mining rewards

--- Blockchain.js
+++ Blockchain.js
@@ -1,13 +1,16 @@
 const Block = require('./Block');
+const Transaction = require('./Transaction');
 
 class Blockchain {
   constructor() {
     this.chain = [this.createGenesisBlock()];
+    this.difficulty = 2;
+    this.pendingTransactions = [];
+    this.miningReward = 100;
   }
 
   createGenesisBlock() {
     return new Block(
-      0,
       new Date(2021, 0, 2), // 2021-01-01
       'Genesis Block',
       '0000000000000000000000000000000000000000000000000000000000000000'
@@ -18,11 +21,44 @@ class Blockchain {
     return this.chain[this.chain.length - 1];
   }
 
-  addBlock(newBlock) {
-    newBlock.previousHash = this.getLatestBlock().hash;
-    newBlock.mineBlock(4);
-    this.chain.push(newBlock);
+  minePendingTransactions(miningRewardAddress) {
+    let block = new Block(Date.now(), this.pendingTransactions);
+    block.mineBlock(this.difficulty);
+    console.log('Block successfully mined');
+    this.chain.push(block);
+
+    this.pendingTransactions = [
+      new Transaction(null, miningRewardAddress, this.miningReward)
+    ];
   }
+
+  createTransaction(transaction) {
+    this.pendingTransactions.push(transaction);
+  }
+
+  getBalanceOfAddress(address) {
+    let balance = 0;
+
+    for (const block of this.chain) {
+      for (const trans of block.transactions) {
+        if (trans.sender === address) {
+          balance -= trans.amount;
+        }
+
+        if (trans.receiver === address) {
+          balance += trans.amount;
+        }
+      }
+    }
+
+    return balance;
+  }
+
+  // addBlock(newBlock) {
+  //   newBlock.previousHash = this.getLatestBlock().hash;
+  //   newBlock.mineBlock(this.difficulty);
+  //   this.chain.push(newBlock);
+  // }
 
   isChainValid() {
     for (let i = 1; i < this.chain.length; i++) {
--- index.js
+++ index.js
@@ -1,30 +1,18 @@
 const Block = require('./Block');
 const Blockchain = require('./Blockchain');
+const Transaction = require('./Transaction');
 
 const blockchain = new Blockchain();
 
-console.log('Mining block 1...')
-const block1 = new Block(
-  1,
-  new Date(),
-  {
-    sender: '1Bf9sZvBHPFGVPX71WX2njhd1NXKv5y7v5',
-    receiver: '3FxUA8godrRmxgUaPv71b3XCUxcoCLtUx2',
-    value: 1.796
-  }
-);
-blockchain.addBlock(block1);
+blockchain.createTransaction(new Transaction('sender', 'receiver', 1.32));
+blockchain.createTransaction(new Transaction('sender', 'receiver2', 14.32));
 
-console.log('Mining block 2...')
-const block2 = new Block(
-  2,
-  new Date(),
-  {
-    sender: '1Bf9sZvBHPFGVPX71WX2njhd1NXKv5y7v5',
-    receiver: '3FxUA8godrRmxgUaPv71b3XCUxcoCLtUx2',
-    value: 1.46
-  }
-);
-blockchain.addBlock(block2);
+console.log('Starting the miner....');
+blockchain.minePendingTransactions('Miner');
+console.log('Balance of Miner is: ' + blockchain.getBalanceOfAddress('Miner'));
+
+blockchain.minePendingTransactions('Miner');
+console.log('Balance of Miner is: ' + blockchain.getBalanceOfAddress('Miner'));
+
 
 console.log(JSON.stringify(blockchain, null, 4));
